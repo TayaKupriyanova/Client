f5ad6b7eb94053e9ac5913c535a26eb5fb2c7f3a
Таисия Куприянова <ktaisochka@mail.ru>
2021-12-19 23:55:36 +0700

    hgfds

--- Client/Connection.cs
+++ Client/Connection.cs
@@ -34,7 +34,7 @@ namespace Client
         public string getFromServer()
         {
             StringBuilder builder = new StringBuilder();
-            byte[] data = new byte[256]; // буфер для ответа
+            byte[] data = new byte[256]; // буфер для ответаf
             int bytes = 0; // количество полученных байт
             do
             {
